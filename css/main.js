860cdf235b23c3646ee3584a53f6f95ddab02b5a
kjy93k <46341006+kjy93k@users.noreply.github.com>
2019-01-21 17:19:22 +0900

    20190121

---
+++ css/main.js
@@ -0,0 +1,26 @@
+
+#fullpage{width: 100%;height: 100%;}
+h1{position: absolute;top: 10px;left: 10px;z-index: 2;}
+h1>a{display: block;text-indent: -999999px;background: url(images/main/common/jeep-logo.png) 0 0 no-repeat;width: 87px;height: 35px;background-size: 100%;}
+#nav{width: 100%;position: absolute;z-index: 1;background-color: #000;}
+#nav ul{float: right;margin-right: 30px;width: 330px;}
+#nav ul:after{content: '';display: block;clear: both;}
+#nav ul li{float: left;color: #fff;width: 25%;transition: .4s;}
+#nav ul li.active{background-color: #fff;}
+#nav ul li.active a{color: #000;font-weight: bold;}
+#nav ul li a{display: block;width: 100%;text-align: center;line-height: 55px;padding: 0 10px;}
+.pages{width: 100%;height: 100%;background-repeat: no-repeat;background-position: center top;background-size: cover;}
+	.interval{width: 100%;height: 100%;}
+	.interval ul{width: 100%;height: 100%;}
+	.interval ul li{width: 100%;height: 100%;}
+	.interval ul li.interval1{background: url(images/main/visual/jeep-main-img-1-1.jpg) center no-repeat;display: none;}
+	.interval ul li.interval2{background: url(images/main/visual/jeep-main-img-1-2.jpg) top no-repeat;display: none;}
+	.interval ul li.interval3{background: url(images/main/visual/jeep-main-img-1-3.jpg) center no-repeat;display: none;}
+	.interval ul li.interval4{background: url(images/main/visual/jeep-main-img-1-4.jpg) center no-repeat;display: none;}
+#page2{background: url(images/main/visual/jeep-main-img-2.jpg) center no-repeat;}
+#page3{background: url(images/main/visual/jeep-main-img-3.jpg) center no-repeat;}
+.slide:nth-child(1){background: url(images/main/visual/jeep-main-img-11.jpg) center no-repeat;}
+.slide:nth-child(2){background: url(images/main/visual/jeep-main-img-12.jpg) center no-repeat;}
+.slide:nth-child(3){background: url(images/main/visual/jeep-main-img-13.jpg) center no-repeat;}
+.slide:nth-child(4){background: url(images/main/visual/jeep-main-img-14.jpg) center no-repeat;}
+.slide:nth-child(5){background: url(images/main/visual/jeep-main-img-15.jpg) center no-repeat;}
