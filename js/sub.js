5b05b233b61e2fc658711c82bb98fd3d1a2892bd
kjy93k <46341006+kjy93k@users.noreply.github.com>
2019-01-22 16:49:42 +0900

    20190122

--- js/sub.js
+++ js/sub.js
@@ -1,25 +1,7 @@
 $(document).ready(function(){
-	var $current=0;
 	var sidenum=0;
 	var sidenum2=0;
 	var sidenum3=0;
-
-	$('#fullpage').fullpage({
-	anchor:["1st","2st","3st","4st"],
-	menu:"#menu"
-	});
-	$('.interval ul li').eq(0).fadeIn(0).delay(7800).fadeOut(200);
-	timer();
-	function timer(){
-		setInterval(function(){
-			if ($current<=2) {
-				$current++;
-			} else {
-				$current=0;
-			}
-			$('.interval ul li').eq($current).fadeIn(200).delay(7600).fadeOut(200);
-		},8000)
-	};
 
 	$('#side-nav').hide();
 	$('.sidemenu').click(function(e){
